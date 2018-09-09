556f52777fe90b294bdc98f2f5ef4ccb4a078291
Rasul <abuhasub@gmail.com>
2018-09-09 20:13:48 +0200

    Show nested ayahs for surah ec /api/quran/1

--- quran/serializers.py
+++ quran/serializers.py
@@ -3,11 +3,20 @@
 be easily rendered to JSON XML or other content types '''
 
 from rest_framework import serializers
-from .models import Surah
+from .models import Surah, Ayah
+
+
+class AyahSerializer(serializers.ModelSerializer):
+    '''Serializer for Ayahs'''
+    class Meta:
+        model = Ayah
+        fields = '__all__'
 
 
 class SurahSerializer(serializers.ModelSerializer):
     '''Serializer for Surah'''
+    ayahs = AyahSerializer(many=True, read_only=True)
+
     class Meta:
         model = Surah
-        fields = '__all__'
+        fields = ('surah_id', 'surah_name', 'surah_ayah_count', 'ayahs')
