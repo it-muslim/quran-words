d90211794e9ba321754dc976af812c82aeee9a24
Rasul <abuhasub@gmail.com>
2018-09-09 17:11:05 +0200

    Use proper outline for script

--- helpers/surah/convert_surah_json.py
+++ helpers/surah/convert_surah_json.py
@@ -5,22 +5,27 @@ to django fixtures file'''
 import json
 from os import path
 
-fixture_dir = path.abspath(
-    path.join(path.dirname(__file__), '../../quran/fixtures'))
-fixture_filename = 'surah_data_init.json'
-fixture_file = path.join(fixture_dir, fixture_filename)
 
-with open('surah.json', encoding='utf-8') as data_file:
-    data = json.load(data_file)
+def main():
+    fixture_dir = path.abspath(
+        path.join(path.dirname(__file__), '../../quran/fixtures'))
+    fixture_filename = 'surah_data_init.json'
+    fixture_file = path.join(fixture_dir, fixture_filename)
 
-surah_data = [
-    {
-        "model": "quran.Surah",
-        "fields": {
-            "surah_name": element['title'],
-            "surah_ayah_count": element['count']}
-    }
-    for element in data]
+    with open('surah.json', encoding='utf-8') as data_file:
+        data = json.load(data_file)
 
-with open(fixture_file, 'w') as fp:
-    json.dump(surah_data, fp, sort_keys=True, indent=4)
+    surah_data = [
+        {
+            "model": "quran.Surah",
+            "fields": {
+                "surah_name": element['title'],
+                "surah_ayah_count": element['count']}
+        }
+        for element in data]
+
+    with open(fixture_file, 'w') as fp:
+        json.dump(surah_data, fp, sort_keys=True, indent=4)
+
+if __name__ == "__main__":
+    main()
