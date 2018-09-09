d570e81b71def63729fcb7c9d3ea68001eb16c0d
Rasul <abuhasub@gmail.com>
2018-09-09 16:48:01 +0200

    use quranwords folder to store settings

--- quranwords/settings.py
+++ quranwords/settings.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'src.urls'
+ROOT_URLCONF = 'quranwords.urls'
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'src.wsgi.application'
+WSGI_APPLICATION = 'quranwords.wsgi.application'
 
 
 # Database
