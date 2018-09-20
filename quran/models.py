acda577ee8d2f8adcf2efbb0093212e082ef1fbd
Rasul <abuhasub@gmail.com>
2018-09-20 21:24:34 +0200

    Refactor, remove Primary keys from model, use Textfield for words.

--- quran/models.py
+++ quran/models.py
@@ -1,23 +1,17 @@
 '''Models describing abstractions used at Quran app'''
 from django.db import migrations, models
-from django.contrib.postgres.fields import ArrayField
 
 
 class Surah(models.Model):
     '''General information about Surahs'''
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     total_ayahs = models.PositiveIntegerField(default=0)
 
 
 class Ayah(models.Model):
     '''General information about Ayah'''
-    id = models.AutoField(primary_key=True, serialize=False)
     ayah = models.PositiveIntegerField(default=0)
-    text = ArrayField(
-        models.CharField(max_length=255, blank=True),
-        blank=True,
-        null=True,)
+    text = models.TextField()
     surah = models.ForeignKey(
         Surah,
         on_delete=models.CASCADE,
