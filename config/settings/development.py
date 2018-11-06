a2039f2191929f5da8cea77b29614245a3e945a0
Azat Mannanov <maaz1313@gmail.com>
2018-11-06 19:18:51 +0500

    Some fixes

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,3 +1,11 @@
 from .base import *
 
 DEBUG = True
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
