3739c0245aafa7a4c4270fe306d79ed642ba1356
Azat Mannanov <maaz1313@gmail.com>
2018-11-13 20:53:25 +0500

    Reciter and Recitation models were changed

--- quran/models.py
+++ quran/models.py
@@ -1,4 +1,5 @@
 '''Models describing abstractions used at Quran app'''
+from os import path
 from django.db import models
 from django.template.defaultfilters import slugify
 
@@ -30,38 +31,58 @@ class Ayah(models.Model):
 class Reciter(models.Model):
     '''Model representing a reciter'''
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
+    quality = models.CharField(max_length=20, blank=True)
+    style = models.CharField(max_length=100, blank=True)
+    slug = models.SlugField()
 
     def save(self, *args, **kwargs):
-        if not self.id:
-            # Newly created object, so set slug
-            self.slug = slugify(self.name)
+        self.quality = self.quality.replace('/', 'p').replace(' ', '')
+        self.slug = slugify(self.name)
         super(Reciter, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 
+def audio_directory_path(instance, filename):
+    '''
+    the function generates path for
+    uploading audio file of ayahs to
+    MEDIA_ROOT/<reciter-slug>/<style>/<quality>/<surah-id>/<ayah-id>.mp3
+    '''
+    ayah = Ayah.objects.get(pk=instance.ayah_id)
+    surah_number = ayah.surah.number
+    file_extension = path.splitext(path.basename(filename))[1]
+    style_dir = path.join(
+        '/', instance.reciter.style
+    ) if instance.reciter.style else ''
+    quality_dir = path.join(
+        '/', instance.reciter.quality
+    ) if instance.reciter.quality else ''
+    return '{slug}{style}{quality}/{surah}/{ayah}.{extension}'.format(
+        slug=instance.reciter.slug,
+        style=style_dir,
+        quality=quality_dir,
+        surah=surah_number,
+        ayah=ayah.number,
+        extension=file_extension
+    )
+
+
 class Recitation(models.Model):
-    '''Model representing a recitation of the reciter'''
-    url_mask = models.URLField(max_length=250)
+    '''
+    This model represents a recitation of an ayah from the Qur'an.
+    Each recitation has an audio file and time segments which hold
+    an information about times when each word in this audio file
+    was pronounced.
+    '''
+    ayah_id = models.PositiveIntegerField()
+    segments = models.TextField()
+    audio = models.FileField(upload_to=audio_directory_path)
     reciter = models.ForeignKey(
         Reciter,
         on_delete=models.CASCADE,
         related_name='recitations')
 
     def __str__(self):
-        return "%s:(%s)" % (self.reciter, self.url_mask)
-
-
-class TimeCode(models.Model):
-    '''Model representing a timecodes of the recitation ayahs'''
-    ayah_id = models.PositiveIntegerField()
-    segments = models.TextField()
-    recitation = models.ForeignKey(
-        Recitation,
-        on_delete=models.CASCADE,
-        related_name='timecodes')
-
-    def __str__(self):
-        return "%s:(%s)" % (self.ayah_id, self.segments)
+        return "%s:(%s)" % (self.reciter, self.ayah_id)
