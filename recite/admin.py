df26a856a98cdc09c3d0bfca99f8c6445410f451
Rasul <abuhasub@gmail.com>
2019-02-06 15:22:04 +0100

    Inline form basic

--- recite/admin.py
+++ recite/admin.py
@@ -1,8 +1,28 @@
+from django import forms
 from django.contrib import admin
 
-from .forms import ReciteForm
 from .models import Reciter, Recitation
 
 
-admin.site.register(Reciter)
-admin.site.register(Recitation)
+class ReciterForm(forms.ModelForm):
+    class Meta:
+        model = Reciter
+        fields = '__all__'
+
+
+class RecitationForm(forms.ModelForm):
+    class Meta:
+        model = Recitation
+        exclude = ('ayah', 'segments')
+
+
+class RecitationInline(admin.TabularInline):
+    model = Recitation
+    form = RecitationForm
+
+
+@admin.register(Reciter)
+class ReciterAdmin(admin.ModelAdmin):
+    inlines = [
+        RecitationInline,
+    ]
