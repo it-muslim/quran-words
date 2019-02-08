aa10faf906264ba344908a914dbd96c0785c4cda
Rasul <abuhasub@gmail.com>
2019-02-08 16:30:44 +0100

    Admin with csv file field

--- recite/admin.py
+++ recite/admin.py
@@ -1,24 +1,30 @@
 from django import forms
 from django.contrib import admin
 
-from .models import Reciter, Recitation
+from quran.models import Ayah, Surah
 
-
-class ReciterForm(forms.ModelForm):
-    class Meta:
-        model = Reciter
-        fields = '__all__'
-
-
-class RecitationForm(forms.ModelForm):
-    class Meta:
-        model = Recitation
-        exclude = ('ayah', 'segments')
+from .models import Recitation, Reciter
 
 
 class RecitationInline(admin.TabularInline):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
     model = Recitation
-    form = RecitationForm
+
+
+class ReciterForm(forms.ModelForm):
+    csv_file = forms.FileField()
+
+    class Meta:
+        model = Reciter
+        fields = '__all__'
 
 
 @admin.register(Reciter)
@@ -26,3 +32,12 @@ class ReciterAdmin(admin.ModelAdmin):
     inlines = [
         RecitationInline,
     ]
+    form = ReciterForm
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        csv_file = form.cleaned_data['csv_file']
+        recitation, _ = Recitation.objects.get_or_create(
+            ayah=Ayah(1, 1),
+            reciter=obj,
+            segments="3:4,5:7")
