54dcf9b046ad6525ebffe2d6be1f7dcd59d94934
Rasul <abuhasub@gmail.com>
2019-02-10 21:11:36 +0100

    Tests for Recitation models

--- recite/tests/test_models.py
+++ recite/tests/test_models.py
@@ -1,5 +1,12 @@
+import shutil
+import tempfile
+
+from django.conf import settings
+from django.core.files import File
 from django.test import TestCase
-from recite.models import Reciter
+
+from quran.models import Ayah
+from recite.models import Recitation, Reciter
 
 
 class ReciterTestCase(TestCase):
@@ -32,3 +39,34 @@ class ReciterTestCase(TestCase):
 
     def test_reciter_slug(self):
         self.assertEqual(self.mishari.slug, "mishari-rashid-al-afasi")
+
+
+class RecitationTestCase(TestCase):
+    """Test for Recitation model"""
+
+    def setUp(self):
+        # Create a temporary directory
+        self.test_dir = tempfile.mkdtemp()
+        self._original_media_root = settings.MEDIA_ROOT
+        settings.MEDIA_ROOT = self.test_dir
+        # get the second ayah from the surah Al-Baqara
+        self.ayah = Ayah.objects.get(pk=8)
+        # create a temporary audio
+        file = tempfile.NamedTemporaryFile(suffix=".mp3")
+        self.audio_ayah = File(file, name=file.name)
+        # create a reciter
+        self.mishari = Reciter.objects.create(name="Mishari Rashid al-ʿAfasi")
+
+    def tearDown(self):
+        # Delete a temporary directory
+        shutil.rmtree(self.test_dir)
+        settings.MEDIA_ROOT = self._original_media_root
+
+    def test_recitation_audio_path_with_name_only(self):
+        recitation = Recitation.objects.create(
+            ayah=self.ayah,
+            segments="",
+            reciter=self.mishari,
+            audio=self.audio_ayah,
+        )
+        self.assertEqual(recitation.audio, "recite/1/002001.mp3")
