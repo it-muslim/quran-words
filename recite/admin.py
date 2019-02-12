97190d7bd4e41a69a80dce642a07a73452b7c271
Rasul <abuhasub@gmail.com>
2019-02-12 21:22:50 +0100

    Process zip files for Recitation audio

--- recite/admin.py
+++ recite/admin.py
@@ -1,6 +1,7 @@
 import csv
 import io
 import json
+from zipfile import ZipFile
 
 from django import forms
 from django.contrib import admin
@@ -8,6 +9,10 @@ from django.contrib import admin
 from quran.models import Ayah
 
 from .models import Recitation, Reciter
+import os
+import tempfile
+import shutil
+from django.core.files import File
 
 
 class RecitationInline(admin.TabularInline):
@@ -28,8 +33,7 @@ class RecitationInline(admin.TabularInline):
 class ReciterForm(forms.ModelForm):
     """Reciter Form has additional segments_file field of Recitations model"""
     segments_file = forms.FileField()
-    audio_files = forms.FileField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    audio_zip_file = forms.FileField()
 
     class Meta:
         model = Reciter
@@ -38,7 +42,11 @@ class ReciterForm(forms.ModelForm):
 
 @admin.register(Reciter)
 class ReciterAdmin(admin.ModelAdmin):
-    """Reciter admin page including Recitations"""
+    """
+    Reciter admin page including Recitations
+    Modified save_model method allows to save Recitations
+    objects at the same model creation form
+    """
     inlines = [
         RecitationInline,
     ]
@@ -47,12 +55,17 @@ class ReciterAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
 
+        # process csv file uploaded
         segments_file = form.cleaned_data['segments_file'].file
         csv_content = self.handle_csv_data(
             segments_file, encoding=request.encoding)
 
-        files = request.FILES.getlist('audio_files')
+        # process audio zip file uploaded
+        audio_zip_file = form.cleaned_data['audio_zip_file']
+        temp_dir = self.get_unzipped_files_dir(audio_zip_file)
+        file_paths = self.get_file_paths(temp_dir)
 
+        # create Recitation objects from processed data
         for row in csv_content:
             segments_list = json.loads(row.get("segments"))
             segments = ",".join(
@@ -60,14 +73,29 @@ class ReciterAdmin(admin.ModelAdmin):
             )
 
             ayah = self.get_ayah_object_from_row(row)
-            file_ayah = self.get_file_audio(files, ayah)
+            file_ayah_path = self.get_file_audio_path(file_paths, ayah)
 
-            recitation, _ = Recitation.objects.get_or_create(
-                ayah=ayah,
-                reciter=obj,
-                segments=segments,
-                audio=file_ayah
-            )
+            with open(file_ayah_path, 'rb') as f:
+                file_ayah = File(f)
+
+                recitation, _ = Recitation.objects.get_or_create(
+                    ayah=ayah,
+                    reciter=obj,
+                    segments=segments,
+                    audio=file_ayah
+                )
+
+        shutil.rmtree(temp_dir)
+
+    def get_file_paths(self, temp_dir):
+        """Return file paths from temp_dir checking subdirectories"""
+        file_paths = []
+        for root, directories, files in os.walk(temp_dir):
+            for filename in files:
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)
+
+        return file_paths
 
     def get_ayah_object_from_row(self, row):
         """Getting Ayah object for Recitation from csv file row"""
@@ -80,12 +108,14 @@ class ReciterAdmin(admin.ModelAdmin):
         )
         return ayah
 
-    def get_file_audio(self, files, ayah):
-        """Loop through uploading files and find corresponding ayah"""
+    def get_file_audio_path(self, file_paths, ayah):
+        """Search in uploaded files for corresponding ayah"""
+
+        file_ayah_name_expected = str(ayah) + '.mp3'
         file_ayah = None
-        for file_mp3 in files:
-            if str(ayah) + '.mp3' == str(file_mp3):
-                file_ayah = file_mp3
+        for file_path in file_paths:
+            if file_ayah_name_expected == os.path.basename(file_path):
+                file_ayah = file_path
                 break
 
         if not file_ayah:
@@ -102,3 +132,11 @@ class ReciterAdmin(admin.ModelAdmin):
         reader = sorted(reader, key=lambda x: int(x['sura']))
 
         return reader
+
+    def get_unzipped_files_dir(self, audio_zip_file):
+        audio_files = ZipFile(audio_zip_file, 'r')
+
+        temp_dir = tempfile.mkdtemp()
+        audio_files.extractall(temp_dir)
+
+        return temp_dir
