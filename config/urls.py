d9d7a16f4e6807b63713371b3ef33fd651712302
Rasul <abuhasub@gmail.com>
2019-03-10 23:18:25 +0100

    Media available by link on development server

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from rest_framework import routers
 from django.urls import path, include
+from django.conf import settings
 
 from quran.api import views as quran_views
 
@@ -12,3 +13,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url('api/', include((router.urls, 'api'), namespace='api')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
