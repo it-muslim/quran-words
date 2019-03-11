4bd8f2eb4046ed896db98fb8fa27fc67de8fbbd2
An4ik <akhmetov.anuar09@gmail.com>
2019-03-11 21:09:43 +0600

    resolved discussions, mostly removed excess tests, refactoring

--- quran/api/tests/views/test_surah_list_retrieve_view.py
+++ quran/api/tests/views/test_surah_list_retrieve_view.py
@@ -8,28 +8,21 @@ from quran.models import Surah
 
 class TestSurahListRetrieveView(APITestCase):
 
-    def setUp(self):
-        self.list_url = reverse("api:quran-list")
-        self.surah_number = 1
-        self.details_url = reverse("api:quran-detail", kwargs={"number": self.surah_number})
-
-    def test_list_url(self):
-        self.assertEqual(self.list_url, "/api/quran/surah/")
-
-    def test_details_url(self):
-        self.assertEqual(self.details_url, "/api/quran/surah/1/")
-
     def test_list(self):
-        response = self.client.get(self.list_url)
+        MAX_SURAH_NUMBER = 114
+        url = reverse("api:quran-list")
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 114)
+        self.assertEqual(len(response.data), MAX_SURAH_NUMBER)
 
         serializer = SurahListSerializer(Surah.objects.all(), many=True)
         self.assertEqual(response.data, serializer.data)
 
     def test_details(self):
-        response = self.client.get(self.details_url)
+        SURAH_NUMBER = 1
+        url = reverse("api:quran-detail", kwargs={"number": SURAH_NUMBER})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        serializer = SurahDetailsSerializer(Surah.objects.get(number=self.surah_number))
+        serializer = SurahDetailsSerializer(Surah.objects.get(number=SURAH_NUMBER))
         self.assertCountEqual(response.data, serializer.data)
--- quran/api/views.py
+++ quran/api/views.py
@@ -5,13 +5,18 @@ from .serializers import SurahListSerializer, SurahDetailsSerializer
 
 
 class SurahListRetrieveView(viewsets.ReadOnlyModelViewSet):
-    """View that allows to List, Retrieve Surah."""
+    """View that allows to list and retrieve Surah."""
     lookup_field = 'number'
     queryset = Surah.objects.all()
     serializer_class = SurahListSerializer
 
     def get_serializer_class(self):
-        """Returns appreciate serializer class for the given action."""
+        """
+        Returns appreciate serializer class for the given action.
+
+        For action retrieve returns SurahDetailsSerializer
+        For action list returns SurahListSerializer
+        """
         if self.action == "retrieve":
             return SurahDetailsSerializer
         return super().get_serializer_class()
