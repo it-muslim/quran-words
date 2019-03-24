e584241966f7660aac78ce87788d0cec6ec6bde8
Rasul <abuhasub@gmail.com>
2019-03-24 23:44:44 +0100

    Reciter and Recitations api implemented

---
+++ recite/api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from recite.models import Recitation, Reciter
+
+
+class ReciterSerializer(serializers.ModelSerializer):
+    """Serializer for Reciter model."""
+
+    class Meta:
+        model = Reciter
+        fields = '__all__'
+
+
+class RecitationSerializer(serializers.ModelSerializer):
+    """Serializer for Recitation model."""
+
+    # Send surah, ayah numbers instead of their pk
+    ayah = serializers.IntegerField(source='ayah.number', read_only=True)
+    surah = serializers.IntegerField(source='surah.number', read_only=True)
+
+    class Meta:
+        model = Recitation
+        fields = ('surah', 'ayah', 'segments', 'audio', 'reciter')
