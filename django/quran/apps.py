c32659e52e2a585ddb764eacfa44988a7f30e233
Rasul <abuhasub@gmail.com>
2019-04-24 00:51:06 +0200

    Separate django files

---
+++ django/quran/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuranConfig(AppConfig):
+    name = 'quran'
--- recite/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReciteConfig(AppConfig):
-    name = 'recite'
