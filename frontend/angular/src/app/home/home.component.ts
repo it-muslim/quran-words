a2b41cc1a819d08c31fefb09fd2698cbbfedc642
Rasul <abuhasub@gmail.com>
2019-05-26 19:37:57 +0200

    Navbar and Home components

---
+++ frontend/angular/src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  title = 'angular';
+  env = environment;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
