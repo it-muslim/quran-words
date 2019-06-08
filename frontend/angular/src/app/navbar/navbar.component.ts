e2bb5e56db90309cab7de861c2331eec01603b27
Rasul <abuhasub@gmail.com>
2019-06-09 00:24:54 +0200

    Implement navbar with autohide.

--- frontend/angular/src/app/navbar/navbar.component.ts
+++ frontend/angular/src/app/navbar/navbar.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+export class NavbarComponent{
+  showNavbar = true;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  @HostListener('body:mouseleave', ['$event'])
+  MouseLeaveTop(event: MouseEvent) {
+    if (event.clientY <= 0) {
+      this.showNavbar = true;
+    }
+  }
 
+  @HostListener('body:mouseenter', ['$event'])
+  MouseBack(event: MouseEvent) {
+    this.showNavbar = false;
+  }
 }
