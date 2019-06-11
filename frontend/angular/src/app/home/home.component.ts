d1ab06a52985b054097ff7f1763b721562bf4d2c
Rasul <abuhasub@gmail.com>
2019-06-11 23:29:21 +0200

    Retrieve surahs from API.
    Refactor angular models. Play button template.

--- frontend/angular/src/app/home/home.component.ts
+++ frontend/angular/src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RestService } from 'src/app/_services/rest.service';
-import { Reciter } from 'src/app/_models/reciter';
+import { Reciter } from 'src/app/_models/reciter.model';
+import { Surah } from 'src/app/_models/surah.model';
 
 @Component({
   selector: 'app-home',
@@ -14,17 +15,28 @@ export class HomeComponent implements OnInit {
   title = 'angular';
   env = environment;
   reciters: Array<Reciter>;
+  surahs: Array<Surah>;
 
   constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.showReciters();
+    this.showSurahs();
   }
 
   showReciters() {
     this.rest.getReciters()
       .subscribe((result: Array<Reciter>) => {
         this.reciters = result}),
+        (error: any) => {
+          console.log('error', error);
+        }
+  }
+
+  showSurahs() {
+    this.rest.getSurahs()
+      .subscribe((result: Array<Surah>) => {
+        this.surahs = result}),
         (error: any) => {
           console.log('error', error);
         }
