820687412211aba447521dc16760967139bbc784
Rasul <abuhasub@gmail.com>
2019-07-25 00:07:09 +0200

    Add models for Surah, SurahMain, Ayah and Recitation

---
+++ frontend/angular/src/app/_models/ayah.model.spec.ts
@@ -0,0 +1,7 @@
+import { Ayah } from './ayah.model';
+
+describe('Ayah', () => {
+  it('should create an instance', () => {
+    expect(new Ayah()).toBeTruthy();
+  });
+});
---
+++ frontend/angular/src/app/_models/recitation.model.spec.ts
@@ -0,0 +1,7 @@
+import { Recitation } from './recitation.model';
+
+describe('Recitation', () => {
+  it('should create an instance', () => {
+    expect(new Recitation()).toBeTruthy();
+  });
+});
