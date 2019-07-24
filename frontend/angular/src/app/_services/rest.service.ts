7b2783a78676bfa45087564ae97d78ed4cb2c12a
Rasul <abuhasub@gmail.com>
2019-07-25 00:13:03 +0200

    Rest services for get Surahs, one Surah and Recitations from API.

--- frontend/angular/src/app/_services/rest.service.ts
+++ frontend/angular/src/app/_services/rest.service.ts
@@ -3,7 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment'; 
 import { Reciter } from "src/app/_models/reciter.model";
-import { Surah } from "src/app/_models/surah.model";
+import { SurahMain, Surah } from "src/app/_models/surah.model";
+import { Recitation } from "src/app/_models/recitation.model";
+
 
 const endpoint = environment.api_url;
 
@@ -18,7 +20,15 @@ export class RestService {
     return this.http.get<Array<Reciter>>(`${endpoint}reciters/`);
   }
 
-  getSurahs(): Observable<Array<Surah>> {
-    return this.http.get<Array<Surah>>(`${endpoint}surahs/`);
+  getSurahs(): Observable<Array<SurahMain>> {
+    return this.http.get<Array<SurahMain>>(`${endpoint}surahs/`);
+  }
+
+  getSurah(number: number): Observable<Surah> {
+    return this.http.get<Surah>(`${endpoint}surahs/${number}`);
+  }
+
+  getRecitations(reciterId: number, surahNumber: number): Observable<Array<Recitation>> {
+    return this.http.get<Array<Recitation>>(`${endpoint}recitations/?reciter_id=${reciterId}&surah_number=${surahNumber}`);
   }
  }
