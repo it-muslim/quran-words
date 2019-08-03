447c10792cd196547342afa2ad845db9b99387d9
Rasul <abuhasub@gmail.com>
2019-08-03 15:41:27 +0200

    Components use async pipe, subscribtion made in rest services.

--- frontend/angular/src/app/home/home.component.ts
+++ frontend/angular/src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RestService } from 'src/app/_services/rest.service';
-import { Reciter } from 'src/app/_models/reciter.model';
-import { SurahMain } from 'src/app/_models/surah.model';
+import { Reciter } from 'src/app/_models/recite.model';
+import { SurahMain } from 'src/app/_models/quran.model';
 
 @Component({
   selector: 'app-home',
@@ -12,37 +12,15 @@ import { SurahMain } from 'src/app/_models/surah.model';
 })
 
 export class HomeComponent implements OnInit {
-  
-  title = 'angular';
+
   env = environment;
-  reciters: Array<Reciter>;
   surahs: Array<SurahMain>;
   selectedReciterId = 1;
   selectedSurahNumber = 1;
 
-  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
+  constructor(public rest: RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    this.showReciters();
-    this.showSurahs();
-  }
-
-  showReciters() {
-    this.rest.getReciters()
-      .subscribe((result: Array<Reciter>) => {
-        this.reciters = result}),
-        (error: any) => {
-          console.log('error', error);
-        }
-  }
-
-  showSurahs() {
-    this.rest.getSurahs()
-      .subscribe((result: Array<SurahMain>) => {
-        this.surahs = result}),
-        (error: any) => {
-          console.log('error', error);
-        }
   }
 
   trackByFn(index, item) {
--- frontend/angular/src/app/playback/playback.component.ts
+++ frontend/angular/src/app/playback/playback.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from 'src/app/_services/rest.service';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { Recitation } from '../_models/recitation.model';
-import { Surah } from '../_models/surah.model';
-import { Ayah } from '../_models/ayah.model';
+import { Recitation } from '../_models/recite.model';
+import { Surah, Ayah } from '../_models/quran.model';
 
 
 @Component({
@@ -14,36 +13,56 @@ import { Ayah } from '../_models/ayah.model';
 
 export class PlaybackComponent implements OnInit {
 
-  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
+  constructor(public rest: RestService, private route: ActivatedRoute, private router: Router) { }
 
   surahRecitation: Array<Recitation>;
   surah: Surah;
-  ayahs: Array<Ayah>;
+  currentAyah: Ayah;
 
   ngOnInit() {
-    let surahNumber = +this.route.snapshot.paramMap.get('surahNumber') || 1;
-    let reciterId = +this.route.snapshot.paramMap.get('reciterId') || 1;
+    const surahNumber = +this.route.snapshot.paramMap.get('surahNumber') || 1;
+    const reciterId = +this.route.snapshot.paramMap.get('reciterId') || 1;
+
     this.getRecitations(surahNumber, reciterId);
     this.getAyahs(surahNumber);
+
   }
 
   getRecitations(surahNumber: number, reciterId: number) {
     this.rest.getRecitations(surahNumber, reciterId)
       .subscribe((result: Array<Recitation>) => {
-        this.surahRecitation = result}),
+        this.surahRecitation = result;
+      },
         (error: any) => {
           console.log('error', error);
-        }
+        });
   }
 
   getAyahs(surahNumber: number) {
     this.rest.getSurah(surahNumber)
       .subscribe((result: Surah) => {
-        this.surah = result
-        this.ayahs = this.surah.ayahs
-      }),
+        this.surah = result;
+        console.log(this.surah.ayahs);
+      },
         (error: any) => {
           console.log('error', error);
-        }
+        });
   }
+
+  playAyahs() {
+    let delay = 0;
+
+    this.surah.ayahs.forEach((ayah: Ayah) => {
+      setTimeout(() => {
+        this.currentAyah = ayah;
+      }, delay);
+
+      const duration = this.surahRecitation
+        .find(recitation => {
+          return recitation.ayah === ayah.number;
+        }).duration;
+      delay += duration;
+    });
+  }
+
 }
