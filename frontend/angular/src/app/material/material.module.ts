ec4fe911d1ba22c21a0aed34cd06d26ac2775779
Rasul <abuhasub@gmail.com>
2019-08-17 15:27:10 +0200

    Update angular to v8

--- frontend/angular/src/app/material/material.module.ts
+++ frontend/angular/src/app/material/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import {
-  MatButtonModule, MatIconModule, MatToolbarModule, MatSelectModule,
-  MatFormFieldModule, MatCardModule, MatGridListModule, MatSidenavModule, MatListModule, MatInputModule, MatSliderModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const material = [
   MatButtonModule,
