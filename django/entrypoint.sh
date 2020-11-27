44ed1235b23b879772296d1d314624629787f35d
Rasul A <abuhasub@gmail.com>
2020-11-27 23:39:51 +0100

    Wait for docker database to initialize before start.

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q'; do
+  sleep 2
+done
+
 python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
