289bfb539a4a86ef379b68d4e8f590e79f362501
Rasul A <abuhasub@gmail.com>
2020-12-29 22:07:30 +0100

    Update dockerfiles and docker-compose to use volumes.
    Update nginx configuration. Store images in private repository.

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q'; do
-  sleep 2
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
 done
 
 python3 manage.py collectstatic --no-input
